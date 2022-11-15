4c238f45d3faeb6723a0134b423bd32ff0ac739e
JonLedo <jonledo.code@gmail.com>
2022-11-15 15:30:29 -0500

    numeric filters added in. uses regex and an operator map

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,8 @@ const Product = require('../models/product')
 
 // for testing
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}) // find func from mongoose docs
-    .sort('name') // sort alphabetically
+  const products = await Product.find({ price: { $gt: 30 } }) // find func from mongoose docs
+    .sort('price') // sort alphabetically
     .select('name price') // only shows these values
     .limit(10) // limit hits per page
   res.status(200).json({ products, nbHits: products.length })
@@ -13,7 +13,7 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
   // destructure only the queries our DB cares about
   // removes errors if unknown keys are searched for
-  const { featured, company, name, sort, fields } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
 
   // create a new query Object to pass to the find
   const queryObject = {}
@@ -29,8 +29,32 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' }
   }
 
+  if (numericFilters) {
+    //operator map
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+
+    const regEx = /\b(>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(regEx, (match) => {
+      return `-${operatorMap[match]}-`
+    })
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
+
   // If user searches for param that doesnt exist, then the query Object is empty.
   // When mongoose searches using an empty object, it still returns ALL items
+  console.log(queryObject)
   let result = Product.find(queryObject)
 
   // sorting
