d44263949fd9cd05f83ff700fa343eb93df3cf0e
JonLedo <jonledo.code@gmail.com>
2022-11-15 13:33:18 -0500

    updated the controllers to test the DB search functionality. Created a query Object in the getAllProducts to control how the user searches for items. Search params that don't exist no longer throw errors

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,29 @@
+const { query } = require('express')
+const Product = require('../models/product')
+
+// for testing
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors')
-  res.status(200).json({ msg: 'products testing route' })
+  const products = await Product.find({
+    featured: true,
+  }) // find func from mongoose docs
+  res.status(200).json({ products, nbHits: products.length })
 }
 
+// real
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' })
+  // destructure only the queries our DB cares about
+  // removes errors if unknown keys are searched for
+  const { featured } = req.query
+  // create a new query Object to pass to the find
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+
+  // If user searches for param that doesnt exist, then the query Object is empty.
+  // When mongoose searches using an empty object, it still returns ALL items
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
